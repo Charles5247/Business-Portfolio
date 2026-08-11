cca7826b954c0b2373f85b6c237c87cdaa6a9f30
unknown <cjemeka2015@gmail.com>
2026-08-11 18:30:37 +0100

    fixes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-1import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import {
   SUPABASE_URL,
   SUPABASE_ANON_KEY,
